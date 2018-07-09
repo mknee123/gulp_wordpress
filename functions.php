ed0fee9550344bdd8585b8afa7f8a8b7afd8212b
MurKat <mknee123@gmail.com>
2018-07-09 13:49:59 -0400

    second commit

--- functions.php
+++ functions.php
@@ -124,9 +124,9 @@ function gulp_wordpress_scripts() {
 	/* ADDED BY MK FOR GULP TESTING */
 	 wp_enqueue_script( 'gulp-wordpress-javascript', get_template_directory_uri() . '/js/app.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'gulp-wordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	//wp_enqueue_script( 'gulp-wordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'gulp-wordpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+//	wp_enqueue_script( 'gulp-wordpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 require('es6-promise').polyfill();
 var gulp = require('gulp');
+var rtlcss = require('gulp-rtlcss');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var gutil = require('gulp-util');
+
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-uglifycss'); // Minifies CSS files
+
+var imagemin = require('gulp-imagemin');
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 var onError = function (err) {
   console.log('An error occurred:', gutil.colors.magenta(err.message));
@@ -10,14 +22,18 @@ var onError = function (err) {
   this.emit('end');
 };
 
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
+// Css
+gulp.task('css', function() {
+  return gulp.src('./styles/**/*.css')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(minifycss())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./'))
 
-var imagemin = require('gulp-imagemin');
-
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+  .pipe(rtlcss())                     // Convert to RTL
+  .pipe(rename({ basename: 'rtl' }))  // Rename to rtl.css
+  .pipe(gulp.dest('./'));             // Output RTL stylesheets (rtl.css)
+});
 
 gulp.task('js', function() {
   return gulp.src(['./js/*.js'])
@@ -43,9 +59,10 @@ gulp.task('watch', function() {
     proxy: 'http://localhost',
   });
 
+  gulp.watch('./styles/**/*.css', ['css', reload]);
   gulp.watch('./js/*.js', ['js', reload]);
   gulp.watch('images/src/*', ['images', reload]);
 });
 
 // default task
-gulp.task('default', [ 'js', 'images', 'watch']);
+gulp.task('default', [ 'css', 'js', 'images', 'watch']);
